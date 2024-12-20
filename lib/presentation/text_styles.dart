5c67e902658825d594a3e40eacf34cb14e5a1e38
ThursdayWitness <lirkir1256@gmail.com>
2024-12-20 18:31:33 +0500

    minor design update

---
+++ lib/presentation/text_styles.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+const titleStyle = TextStyle(
+  fontFamily: 'DMSans',
+  fontSize: 24,
+);
+
+const subtitleStyle = TextStyle(
+  fontFamily: 'DMSans',
+  fontSize: 11,
+);
+
+const buttonStyle = TextStyle(
+  fontFamily: 'DMSans',
+  fontWeight: FontWeight.w700,
+  fontSize: 14,
+  height: 0.95,
+);
