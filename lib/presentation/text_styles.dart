5a73013c11e5df285f272e0385f63636f05b8ffa
ThursdayWitness <lirkir1256@gmail.com>
2024-12-20 20:10:51 +0500

    removed redundant code

--- lib/presentation/text_styles.dart
+++ lib/presentation/text_styles.dart
@@ -9,10 +9,3 @@ const subtitleStyle = TextStyle(
   fontFamily: 'DMSans',
   fontSize: 11,
 );
-
-const buttonStyle = TextStyle(
-  fontFamily: 'DMSans',
-  fontWeight: FontWeight.w700,
-  fontSize: 14,
-  height: 0.95,
-);
